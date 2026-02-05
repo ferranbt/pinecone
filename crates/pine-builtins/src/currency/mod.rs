557b4a8539795fe59aec9374cb38c0b21c6e0b3f
Ferran Borreguero <ferran.borreguero@gmail.com>
2026-02-05 18:49:35 +0800

    Add currency constants

--- crates/pine-builtins/src/currency/mod.rs
+++ crates/pine-builtins/src/currency/mod.rs
@@ -9,12 +9,11 @@ pub fn register() -> Value {
 
     // All currency constants as string values
     let currencies = [
-        "AED", "ARS", "AUD", "BDT", "BHD", "BRL", "BTC", "CAD", "CHF", "CLP",
-        "CNY", "COP", "CZK", "DKK", "EGP", "ETH", "EUR", "GBP", "HKD", "HUF",
-        "IDR", "ILS", "INR", "ISK", "JPY", "KES", "KRW", "KWD", "LKR", "MAD",
-        "MXN", "MYR", "NGN", "NOK", "NONE", "NZD", "PEN", "PHP", "PKR", "PLN",
-        "QAR", "RON", "RSD", "RUB", "SAR", "SEK", "SGD", "THB", "TND", "TRY",
-        "TWD", "USD", "USDT", "VES", "VND", "ZAR",
+        "AED", "ARS", "AUD", "BDT", "BHD", "BRL", "BTC", "CAD", "CHF", "CLP", "CNY", "COP", "CZK",
+        "DKK", "EGP", "ETH", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KES",
+        "KRW", "KWD", "LKR", "MAD", "MXN", "MYR", "NGN", "NOK", "NONE", "NZD", "PEN", "PHP", "PKR",
+        "PLN", "QAR", "RON", "RSD", "RUB", "SAR", "SEK", "SGD", "THB", "TND", "TRY", "TWD", "USD",
+        "USDT", "VES", "VND", "ZAR",
     ];
 
     for currency in currencies {
