92a2e9c89e3e0723cd67453d7ccbecf42a690ea1
Ferran Borreguero <ferran.borreguero@gmail.com>
2026-08-30 11:37:55 +0200

    Use SeriesBuffer everywhere (#182)
    * Simplify benchmark
    
    * Use SeriesBuffer for all historical vectors
    
    * Fix lint

--- benches/src/interpreter.rs
+++ benches/src/interpreter.rs
@@ -1,24 +1,11 @@
-mod test_data;
-
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use pine_lang::core::DefaultPineOutput;
-use pine_lang::{execute, ScriptBuilder};
-use test_data::generate_bars;
+use pine_core::MAX_LOOKBACK;
+use pine_lang::execute;
+use pinecone_benches::{generate_bars, TEST_SCRIPTS};
 
-const TEST_SCRIPTS: &[(&str, &str)] = &[
-    ("simple", include_str!("../test_data/simple.pine")),
-    (
-        "moving_averages",
-        include_str!("../test_data/moving_averages.pine"),
-    ),
-    ("rsi", include_str!("../test_data/rsi.pine")),
-    ("macd", include_str!("../test_data/macd.pine")),
-    ("complex", include_str!("../test_data/complex.pine")),
-];
-
-fn bench_single_bar(c: &mut Criterion) {
+fn bench_full_run(c: &mut Criterion) {
     let mut group = c.benchmark_group("interpreter/single_bar");
-    let data = generate_bars(200); // Generate enough bars for historical lookback
+    let data = generate_bars(MAX_LOOKBACK * 2);
 
     for (name, source) in TEST_SCRIPTS {
         group.bench_with_input(BenchmarkId::from_parameter(name), source, |b, source| {
@@ -31,22 +18,5 @@ fn bench_single_bar(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_compile_only(c: &mut Criterion) {
-    let mut group = c.benchmark_group("interpreter/compile");
-
-    for (name, source) in TEST_SCRIPTS {
-        group.bench_with_input(BenchmarkId::from_parameter(name), source, |b, source| {
-            b.iter(|| {
-                let _ = ScriptBuilder::<DefaultPineOutput>::with_code(black_box(source))
-                    .with_data(pine_lang::core::Data::default())
-                    .compile()
-                    .unwrap();
-            });
-        });
-    }
-
-    group.finish();
-}
-
-criterion_group!(benches, bench_compile_only, bench_single_bar);
+criterion_group!(benches, bench_full_run);
 criterion_main!(benches);
